1aad012765df9744d34e2609e1897e828746f65e
Junel Amelon <junelamelon92@gmail.com>
2026-02-03 08:48:07 +0100

    UPDATE

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,20 +16,32 @@ const baskerville = Libre_Baskerville({
 });
 
 export const metadata: Metadata = {
-  title: 'Le Oui Parfait - Organisation de Mariages',
-  description: 'Organisateur de mariage élégant et professionnel pour créer votre jour parfait',
+  title: 'Le Oui Parfait - Organisation de Mariages Élégants',
+  description: 'Organisateur de mariage professionnel créant des célébrations inoubliables. Planification complète, stylisme de fiançailles et gestion d\'événements sur mesure.',
+  metadataBase: new URL('https://leouiparfait.fr'),
   openGraph: {
+    title: 'Le Oui Parfait - Organisation de Mariages Élégants',
+    description: 'Créez votre mariage de rêve avec notre équipe d\'experts. Planification, stylisme et coordination pour une journée parfaite.',
+    type: 'website',
+    locale: 'fr_FR',
+    siteName: 'Le Oui Parfait',
     images: [
       {
-        url: 'https://bolt.new/static/og_default.png',
+        url: 'https://images.pexels.com/photos/2788488/pexels-photo-2788488.jpeg?auto=compress&cs=tinysrgb&w=1200',
+        width: 1200,
+        height: 630,
+        alt: 'Le Oui Parfait - Organisation de Mariages',
       },
     ],
   },
   twitter: {
     card: 'summary_large_image',
+    title: 'Le Oui Parfait - Organisation de Mariages Élégants',
+    description: 'Créez votre mariage de rêve avec notre équipe d\'experts',
     images: [
       {
-        url: 'https://bolt.new/static/og_default.png',
+        url: 'https://images.pexels.com/photos/2788488/pexels-photo-2788488.jpeg?auto=compress&cs=tinysrgb&w=1200',
+        alt: 'Le Oui Parfait - Organisation de Mariages',
       },
     ],
   },

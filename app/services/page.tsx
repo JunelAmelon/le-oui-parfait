4f3bc678d1aa5f21ee29008d26de566cc49e52c2
Junel Amelon <junelamelon92@gmail.com>
2026-03-20 07:32:48 +0100

    UPDATE

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -11,7 +11,7 @@ export default function ServicesPage() {
       <main>
         <HeroPage
           title="Services"
-          subtitle="Des prestations sur mesure pour faire de votre mariage un moment unique"
+          subtitle="Des prestations sur mesure pour faire de votre mariage un UN moment unique"
           backgroundImage="https://media.abcsalles.com/images/1/articles/960x640/840709/comment-trouver-ses-prestataires-de-mariage.jpg"
         />
 
